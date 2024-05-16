bf78454864e0d6c8b7c053a82f23dc093242d42c
Giuliano Fossatti <giulifossatti@gmail.com>
2024-05-16 17:44:52 -0300

    more tests

--- social/test/test_views.py
+++ social/test/test_views.py
@@ -1,27 +1,41 @@
 from django.test import TestCase, Client
 from django.urls import reverse, resolve
-from social.models import Profile, Post, Follow
+from social.models import Profile, Post, Follow, User
 import json
+from datetime import datetime
 
 
 class TestViews(TestCase):
 
+    def setUp(self):
+        self.client = Client()
+        self.post_url = reverse('post')
+
     def test_signup_view_GET(self):
-        client = Client()
-        response = client.get(reverse('signup'))
+        response = self.client.get(reverse('signup'))
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'form/signup.html')
 
     def test_profile_view_GET(self):
-        client = Client()
-        response = client.get(reverse('profile', args=['username']))
+        response = self.client.get(reverse('profile', args=['username']))
         self.assertEquals(response.status_code, 302)
         self.assertRedirects(
             response, '/login/?next=/profile/username/')
 
     def test_post_view_GET(self):
-        client = Client()
-        response = client.get(reverse('post'))
+        response = self.client.get(self.post_url)
         self.assertEquals(response.status_code, 302)
         self.assertRedirects(
             response, '/login/?next=/post/')
+
+    def test_post_POST(self):
+        user = User.objects.create(
+            username="user_test",
+            password="pass_test"
+        )
+        self.client.force_login(user)
+        response = self.client.post(self.post_url, {'timestamp': datetime.now(),
+                                                    'content': 'content2'})
+        post = Post.objects.filter(content='content2').first()
+        self.assertEquals(response.status_code, 302)
+        self.assertEquals(post.user_id, 1)
--- social_network/settings.py
+++ social_network/settings.py
@@ -125,7 +125,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 LOGIN_REDIRECT_URL = 'feed'
-LOGout_REDIRECT_URL = 'login'
+LOGOUT_REDIRECT_URL = 'login'
 
 
 # Default primary key field type
